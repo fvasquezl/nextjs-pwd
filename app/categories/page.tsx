d91de29b9859dbbe3923b8dafbfb29a3a6c879a9
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2023-09-18 16:02:43 -0700

    categories with paginagion

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -32,16 +32,53 @@ const Categories = () => {
         {filterData &&
           filterData.map((item, index) => <li key={index}>{item.name}</li>)}
       </ul>
-      <ReactPaginate
-        containerClassName={"pagination"}
-        pageClassName={"page-item"}
-        activeClassName={"active"}
-        onPageChange={(event) => setPage(event.selected)}
-        pageCount={Math.ceil(items.length / n)}
-        breakLabel="..."
-        previousLabel="< previous"
-        nextLabel="next >"
-      />
+
+      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+        <div className="flex flex-1 justify-between sm:hidden">
+          <a
+            href="#"
+            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >
+            Previous
+          </a>
+          <a
+            href="#"
+            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+          >
+            Next
+          </a>
+        </div>
+        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+          <div>
+            <p className="text-sm text-gray-700">
+              Showing
+              <span className="font-medium">1</span>
+              to
+              <span className="font-medium">10</span>
+              of
+              <span className="font-medium">97</span>
+              results
+            </p>
+          </div>
+
+          <ReactPaginate
+            containerClassName={
+              "flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6"
+            }
+            pageClassName={
+              "relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+            }
+            activeClassName={
+              "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            }
+            onPageChange={(event) => setPage(event.selected)}
+            pageCount={Math.ceil(items.length / n)}
+            breakLabel="..."
+            previousLabel="< previous"
+            nextLabel="next >"
+          />
+        </div>
+      </div>
     </>
   );
 };
